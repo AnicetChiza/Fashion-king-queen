dbd1ab8f5a01fff6d1db2f31399578289ca3a152
AnicetChiza <chizaanicet@gmail.com>
2024-05-04 16:05:09 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -19,3 +19,31 @@ function changeBg() {
 }
 
 setInterval(changeBg, 4000);
+
+// Événement de chargement de la page
+document.addEventListener('DOMContentLoaded', startValueAnimation);
+// Fonction pour démarrer l'animation des valeurs
+function startValueAnimation() {
+    let valueDisplays = document.querySelectorAll('.num');
+    let interval = 4000;
+
+    valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute('data-Val'));
+        let decimalPart = endValue % 1; // Obtenir la partie décimale
+        let steps = Math.floor(endValue); // Nombre d'étapes entières
+        let stepDuration = interval / steps;
+        let counter = setInterval(function() {
+            if (startValue < steps) {
+                startValue += 1;
+                valueDisplay.textContent = startValue;
+            } else if (startValue === steps && decimalPart > 0) {
+                // Gérer la partie décimale si présente
+                startValue += decimalPart;
+                valueDisplay.textContent = endValue.toFixed(1); // Afficher avec 1 décimale
+            } else {
+                clearInterval(counter);
+            }
+        }, stepDuration);
+    });
+}
