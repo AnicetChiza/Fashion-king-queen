5f8f391f5f28b7a234e40cf1e49d829c4f8939a9
AnicetChiza <chizaanicet@gmail.com>
2024-05-04 13:00:47 +0200

    add to my repository

---
+++ javascript/app.js
@@ -0,0 +1,42 @@
+/*function changeBg() {
+    const images = [
+        'url("../images/home-page/img1.jpg")',
+        'url("../images/home-page/img2.JPG")',
+        'url("../images/home-page/img3.JPG")',
+        'url("../images/home-page/img4.JPG")',
+        'url("../images/home-page/img5.JPG")',
+        'url("../images/home-page/img6.jpg")',
+        'url("../images/home-page/img7.JPG")',
+        'url("../images/home-page/img8.JPG")',
+        'url("../images/home-page/img9.jpg")',
+        'url("../images/home-page/img10.JPG")',
+    ];
+    const page = document.querySelector('.home-page');
+    const bg = images[Math.floor(Math.random() * images.length)];
+    page.style.backgroundImage = bg;
+}
+
+setInterval(changeBg, 1000);*/
+let currentIndex = 0;
+
+function changeBg() {
+    const images = [
+        'url("../images/home-page/img4.jpg")',
+        'url("../images/home-page/img1.jpg")',
+        'url("../images/home-page/img2.JPG")',
+        'url("../images/home-page/img3.JPG")',
+        'url("../images/home-page/img5.JPG")',
+        'url("../images/home-page/img6.jpeg")',
+        'url("../images/home-page/img7.jpeg")',
+        'url("../images/home-page/img8.jpeg")',
+        'url("../images/home-page/img9.jpeg")',
+        'url("../images/home-page/img10.JPG")',
+    ];
+
+    const page = document.querySelector('.home-page');
+    const bg = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+    page.style.backgroundImage = bg;
+}
+
+setInterval(changeBg, 3000);
