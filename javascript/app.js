2798a1f82cd54371be3983666ca0467c077f3ebd
AnicetChiza <chizaanicet@gmail.com>
2024-05-04 13:50:50 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,4 @@
-/*function changeBg() {
-    const images = [
-        'url("../images/home-page/img1.jpg")',
-        'url("../images/home-page/img2.JPG")',
-        'url("../images/home-page/img3.JPG")',
-        'url("../images/home-page/img4.JPG")',
-        'url("../images/home-page/img5.JPG")',
-        'url("../images/home-page/img6.jpg")',
-        'url("../images/home-page/img7.JPG")',
-        'url("../images/home-page/img8.JPG")',
-        'url("../images/home-page/img9.jpg")',
-        'url("../images/home-page/img10.JPG")',
-    ];
-    const page = document.querySelector('.home-page');
-    const bg = images[Math.floor(Math.random() * images.length)];
-    page.style.backgroundImage = bg;
-}
-
-setInterval(changeBg, 1000);*/
+//Home page changing image background
 let currentIndex = 0;
 
 function changeBg() {
@@ -36,4 +18,4 @@ function changeBg() {
     page.style.backgroundImage = bg;
 }
 
-setInterval(changeBg, 3000);
+setInterval(changeBg, 4000);
